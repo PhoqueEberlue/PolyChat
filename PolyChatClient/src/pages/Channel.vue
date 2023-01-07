ab5a21b0b7d0eb44339237b5ba5895a2cf978109
theBigBlase <thebigblase@gmail.com>
2023-01-07 20:22:50 +0100

    oopsies fixes

--- PolyChatClient/src/pages/Channel.vue
+++ PolyChatClient/src/pages/Channel.vue
@@ -7,19 +7,24 @@ export default {
     return {
       // Defining our socket
       socket: io("http://localhost:4500"),
-      users: []
+      users: [],
+			user: "no"
     }
   },
+	mounted(){
+		console.log("user : ", this.user)
+	},
   created() {
     // HARD CODED USER FOR TEST PURPOSES
-    const user = "Melenchon";
     /* ------------- SETUP LISTENERS ------------- */
 
+
+		this.user = $cookies.get("username");
     // Listen greetings of the ~ lovely ~ server UwU
     this.socket.on("greetings", (arg) => {
       console.log("Connected to the server");
       // Send greetings back
-      this.socket.emit("greetings-back", `Wesh wesh cane a peche, c'est moi ${user}`);
+      this.socket.emit("greetings-back", `Wesh wesh cane a peche, c'est moi ${this.user}`);
     })
 
     // Listen the return of the get channel call
@@ -42,20 +47,19 @@ export default {
     /* ------------- Executing on page load requests ------------- */
 
     // Request the channels the user is in
-    this.socket.emit("get-channel", user);
+    this.socket.emit("get-channel", this.user);
 
 
     // The following calls are for test purposes, they need to be summoned by user input
-    this.socket.emit("connect-channel", "1", user); // When clicking on a channel for example (could redirect to a new page or change the current one)
-    this.socket.emit("send-message-channel", user, "1", "Bonjour a tous mes petits gauchistes"); // When pressing enter / send message button
+    this.socket.emit("connect-channel", "1", this.user); // When clicking on a channel for example (could redirect to a new page or change the current one)
+    this.socket.emit("send-message-channel", this.user, "1", "Bonjour a tous mes petits gauchistes"); // When pressing enter / send message button
   }
 }
 </script>
 
 <template>
 	<div>
-		<h3>channel {{ $route.params.id }} </h3>
-		<h3>yes {{ $cookies.get(username) }} </h3>
+		<h3>yes {{ this.user }} </h3>
 	</div>
 </template>
 
